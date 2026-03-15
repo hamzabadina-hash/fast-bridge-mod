221d6fb2f7fba6dcc149db19568aea3add1d332a
hamzabadina-hash <hamzabadina@gmail.com>
2026-03-15 18:32:00 +0000

    Create FastBridgeMod.java

---
+++ src/main/java/com/yourname/fastbridge/FastBridgeMod.java
@@ -0,0 +1,12 @@
+package com.yourname.fastbridge;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FastBridgeMod implements ModInitializer {
+    public static final String MOD_ID = "fastbridge";
+
+    @Override
+    public void onInitialize() {
+        FastBridgeConfig.load();
+    }
+}
